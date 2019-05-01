436b3f63748b32d68f695b235278f6999e9d61c9
Rogerio Batista <rogerrbg@gmail.com>
2019-05-01 15:17:03 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -71,7 +71,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		Produto p1 = new Produto(null, "Computados", 2000.00 );
+		Produto p1 = new Produto(null, "Computador", 2000.00 );
 		Produto p2 = new Produto(null, "Impressora", 800.00 );
 		Produto p3 = new Produto(null, "Mouse", 80.00 );
 		
