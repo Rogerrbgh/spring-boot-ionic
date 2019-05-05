1f83cd411abca81819286b5610434d5f20cb3478
Rogerio Batista <rogerrbg@gmail.com>
2019-05-05 14:37:43 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -70,12 +70,20 @@ public class ItemPedido implements Serializable{
 	}
 	
 	@JsonIgnore
-	public Pedido getPedido () {
+	public Pedido getPedido() {
 		return id.getPedido();
 	}
-		
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 
 	@Override
